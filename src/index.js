18643e68fe80ddad7dbbc2fa48762e66d7d79a43
Scott Lowe <scott@entercastle.com>
2015-08-11 12:50:58 -0400

    fix(index.js): correct JSDoc syntax

--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ var AuthService = function(provider) {
    * @param {string} email the new user's email
    * @param {string} password the new user's password
    * @param {string[]} roles an array of roles, supported values for the array are 'manager', 'tenant', 'owner', and 'contractor'
-   * @param {string[]} optional, roleIDs the record IDs for the roles, the ids must be passed in the same order as the roles in the roles paramater
-   * @param {string} optional, avatarImageID the id for the avatar image for the user
-   * @param {string} permissionID optional the id of the permission record for the user record
+   * @param {string[]} roleIDs (optional) the record IDs for the roles, the ids must be passed in the same order as the roles in the roles paramater
+   * @param {string} avatarImageID (optional) the id for the avatar image for the user
+   * @param {string} permissionID (optional) the id of the permission record for the user record
    * @returns {promise} resolves with the newly created user record
    */
   this.createUser = function(email, password, roles, roleIDs, avatarImageID, permissionID) {
